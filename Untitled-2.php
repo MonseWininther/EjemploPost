54b00f911335768d4bef3f12222f44852a70422b
Carlos Hernandez <minusgat@gmail.com>
2022-05-23 21:36:06 -0500

    init commit

---
+++ Untitled-2.php
@@ -0,0 +1,14 @@
+<?php 
+$nombre=$_POST['nombre'];
+$apellido=$_POST['apellido'];
+$edad =$_POST['edad'];
+$sexo =$_POST['sexo'];
+$estatura =$_POST['estatura'];
+//ejemplo
+//Datos sensibles con el metodo POST
+echo($nombre);
+echo($apellido);
+echo($edad);
+echo($sexo);
+echo($estatura);
+?>
